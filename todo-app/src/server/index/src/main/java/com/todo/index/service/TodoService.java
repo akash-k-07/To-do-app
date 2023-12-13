8fd082ed33caa2298d77349c0dd641c356d68e60
Akash k <akashkari1999@gmail.com>
2023-12-13 16:23:26 +0530

    backend
    created a springbootbackend for the existing todod app

---
+++ todo-app/src/server/index/src/main/java/com/todo/index/service/TodoService.java
@@ -0,0 +1,40 @@
+package com.todo.index.service;
+import com.todo.index.model.Todo;
+import com.todo.index.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+
+    @Autowired
+    private TodoRepository todoRepository;
+
+    public List<Todo> getAllTodos() {
+        return todoRepository.findAll();
+    }
+
+    public Todo createTodo(Todo todo) {
+        return todoRepository.save(todo);
+    }
+
+    public Todo getTodoById(String id) {
+        return todoRepository.findById(id).orElse(null);
+    }
+
+    public Todo updateTodo(String id, Todo updatedTodo) {
+        Todo todo = todoRepository.findById(id).orElse(null);
+        if (todo != null) {
+            todo.setTitle(updatedTodo.getTitle());
+            todo.setCompleted(updatedTodo.isCompleted());
+            return todoRepository.save(todo);
+        }
+        return null;
+    }
+
+    public void deleteTodo(String id) {
+        todoRepository.deleteById(id);
+    }
+}
